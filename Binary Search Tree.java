4a357c2e085864f3ebd83257d21c46eee7f88dda
emredncer <108138943+emredncer@users.noreply.github.com>
2022-09-19 13:04:30 +0300

    Add files via upload

---
+++ Binary Search Tree.java
@@ -0,0 +1,9 @@
+#   [7, 5, 1, 8, 3, 6, 0, 9, 4, 2] dizisinin Binary-Search-Tree aşamalarını yazınız.
+
+#   Örnek: root x'dir. root'un sağından y bulunur. Solunda z bulunur vb.
+#   Cevap
+#   root 6'dır solunda 5 sağında 7 vardır.
+    #   7 nin solunda 6 sağında 9 vardır.
+#   4ün solunda 3 sağında 5 vardır.
+    #   3 ün solunda 2
+            #   2 nin solunda 1 onun solunda da 0 vardır.
